79864211517e690dff998ce55806b141a7f4a623
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2022-03-19 20:55:11 +0100

    Install Deno correctly with Netlify

--- build/netlify.sh
+++ build/netlify.sh
@@ -1,4 +1,6 @@
 curl -fsSL https://deno.land/x/install/install.sh | sh
+export DENO_INSTALL="/opt/buildhome/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
 ./web_bundle.sh
 cp proxy/netlify.toml site/netlify.toml
 cd site
