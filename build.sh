0f918f68bf427e8af3d801d43587e5abdde0dd29
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2022-03-19 17:27:18 +0100

    Update Cloudflare build script

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 # For Cloudflare Pages
 curl -fsSL https://deno.land/x/install/install.sh | sh
+export DENO_INSTALL="/opt/buildhome/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
 ./web_bundle.sh
 cd site
+npm run install
 npm run build
