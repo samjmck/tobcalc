7a316e3453af85ca129c7c74faea184d39a68ddb
samjmckenzie <25828757+samjmckenzie@users.noreply.github.com>
2022-03-28 18:34:26 +0200

    Update netlfiy build script

--- build/netlify.sh
+++ build/netlify.sh
@@ -1,6 +1,7 @@
 curl -fsSL https://deno.land/x/install/install.sh | sh
 export DENO_INSTALL="/opt/buildhome/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
+deno run --allow-net --allow-write scripts/fetch_registered_funds.ts
 ./web_bundle.sh
 cp proxy/netlify.toml site/netlify.toml
 cd site
