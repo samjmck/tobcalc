f2094e4370a6bb39c0f1637070684fb2901e726f
Vivien Boistuaud <vivien.b@outlook.be>
2023-04-04 02:48:14 +0200

    Additional test datasets for Boursorama With Updated, passed, Unit test

--- src/adapters/Boursorama_adapter_test.ts
+++ src/adapters/Boursorama_adapter_test.ts
@@ -9,8 +9,10 @@ Deno.test({
         read: true,
     },
     fn: async () => {
-        const data = await Deno.readFile("src/adapters/Boursorama_adapter_test.csv");
-        const brokerTransactions = await BousoramaAdapter(new Blob([data]));
+        console.log("Testing dataset #1 - src/adapters/Boursorama_adapter_test.csv");
+
+        var data = await Deno.readFile("src/adapters/Boursorama_adapter_test.csv");
+        var brokerTransactions = await BousoramaAdapter(new Blob([data]));
 
         assertEquals(brokerTransactions[0], <BrokerTransaction> {
             date: new Date(2023, 1, 6), // 2023-06-02
@@ -23,6 +25,38 @@ Deno.test({
             isin: "IE00B4L5Y983",
             currency: CurrencyCode.EUR,
             value: 144_66,
+        });
+        assertEquals(brokerTransactions.length, 2);
+
+        console.log("Testing dataset #2 - src/adapters/Boursorama_adapter_test2.csv");
+
+        data = await Deno.readFile("src/adapters/Boursorama_adapter_test2.csv");
+        brokerTransactions = await BousoramaAdapter(new Blob([data]));
+
+        assertEquals(brokerTransactions[0], <BrokerTransaction> {
+            date: new Date(2023, 2, 6), // 2023-03-06
+            isin: "IE00B4L5Y983",
+            currency: CurrencyCode.EUR,
+            value: 219_89,
+        });
+        assertEquals(brokerTransactions.length, 1);
+
+        console.log("Testing dataset #3 - src/adapters/Boursorama_adapter_test3.csv");
+
+        data = await Deno.readFile("src/adapters/Boursorama_adapter_test3.csv");
+        brokerTransactions = await BousoramaAdapter(new Blob([data]));
+
+        assertEquals(brokerTransactions[0], <BrokerTransaction> {
+            date: new Date(2023, 3, 3), // 2023-04-03
+            isin: "LU1781541179",
+            currency: CurrencyCode.EUR,
+            value: 307_28,
+        });
+        assertEquals(brokerTransactions[1], <BrokerTransaction> {
+            date: new Date(2023, 3, 3), // 2023-04-03
+            isin: "IE00B4L5Y983",
+            currency: CurrencyCode.EUR,
+            value: 290_80,
         });
         assertEquals(brokerTransactions.length, 2);
     },
