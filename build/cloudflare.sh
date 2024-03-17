c033b1d039c251d57cd37d4a7bfcb07b63a01141
Sam Mckenzie <25828757+samjmck@users.noreply.github.com>
2024-03-17 12:54:42 +0100

    Change build settings

--- build/cloudflare.sh
+++ build/cloudflare.sh
@@ -1,5 +1,5 @@
 # For Cloudflare Pages
-curl -fsSL https://deno.land/x/install/install.sh | sh
+curl -fsSL https://deno.land/install.sh | sh -s v1.40.2
 export DENO_INSTALL="/opt/buildhome/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 ./web_bundle.sh
--- build/netlify.sh
+++ build/netlify.sh
@@ -1,4 +1,4 @@
-curl -fsSL https://deno.land/x/install/install.sh | sh
+curl -fsSL https://deno.land/install.sh | sh -s v1.40.2
 export DENO_INSTALL="/opt/buildhome/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 deno run --allow-net --allow-write scripts/fetch_registered_funds.ts
