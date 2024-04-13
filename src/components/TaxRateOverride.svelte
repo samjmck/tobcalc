0ed0455057e733bd0c3fb6eb7956e79c40f6d322
Brian Hanquet <apps.brinat@pm.me>
2024-04-13 21:18:36 +0000

    UI Change

--- src/components/TaxRateOverride.svelte
+++ src/components/TaxRateOverride.svelte
@@ -30,9 +30,12 @@
         taxRateOverrides.delete(isin);
     }
 </script>
+<h2>Tax override</h2>
 
-<p>If you notice an incorrect tax rate, you can override the rate by adding an entry in this table.</p>
-<p>Take VWCE for example. If you believe its TOB rate should be 0.12% yet tobcalc is showing 1.32%, then you can manually override this by adding an entry with ISIN "IE00BK5BQT80" and tax rate 0.12%.</p>
+<div class="explication">
+    <p>If you notice an incorrect tax rate, you can override the rate by adding an entry in this table.</p>
+    <p>Take VWCE for example. If you believe its TOB rate should be 0.12% yet tobcalc is showing 1.32%, then you can manually override this by adding an entry with ISIN "IE00BK5BQT80" and tax rate 0.12%.</p>
+</div>
 
 <Table>
     <svelte:fragment slot="head">
@@ -55,8 +58,18 @@
     {#each [...$taxRateOverrides.entries()] as [isin, rate]}
         <tr>
             <td>{isin}</td>
-            <td>{rate}</td>
+            <td>{formatPercentage(rate)}</td>
             <td><button on:click={() => removeEntry(isin)}>Remove</button></td>
         </tr>
     {/each}
 </Table>
+
+<style>
+    .explication {
+        margin-bottom: 1rem;
+    }
+
+    .explication > p{
+        margin-bottom: 0.8em;
+    }
+</style>
